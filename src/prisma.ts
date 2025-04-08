4aa84b99be48fb53a81484fc21b6fae000cc4b66
Pedro Manse <pedro.manse@dmk3.com.br>
2025-04-08 11:12:43 -0300

    better prisma module for nest

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,10 +1,22 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-	async onModuleInit() {
-		await this.$connect();
-	}
+export class PrismaService extends PrismaClient {
+  private isConnected: boolean = false;
+
+  async onModuleInit() {
+    if (!this.isConnected) {
+      await this.$connect();
+      this.isConnected = true;
+    }
+  }
+
+  async onModuleDestroy() {
+    if (this.isConnected) {
+      await this.$disconnect();
+      this.isConnected = false;
+    }
+  }
 }
